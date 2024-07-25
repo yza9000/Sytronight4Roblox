61c23b0c3373b3f7896d9cfa628b96ad38bb74e8
yza9000 <jamaljacks620@gmail.com>
2024-07-25 04:08:31 -0600

    Update 6872265039.lua

--- Module/6872265039.lua
+++ Module/6872265039.lua
@@ -1,1 +1,51 @@
--- can't you wait?
+runcode(function()
+	local antivoidp
+end)
+
+local AntiVoid = World:NewSection("AntiVoid")
+AntiVoid:NewToggle("AntiVoid", "Give's you a second chance to get back on land", function(state)
+	if state then
+		antivoidp = Instance.new("Part", workspace)
+		antivoidp.Name = "AntiVoid"
+		antivoidp.CanCollide = true
+		antivoidp.Size = Vector3.new(2048, 1, 2048)
+		antivoidp.Anchored = true
+		antivoidp.Transparency = 1 - (antivoidtransparent["Value"] / 100)
+		antivoidp.Material = Enum.Material.Neon
+		antivoidp.Color = Color3.fromHSV(antivoidcolor["Hue"], antivoidcolor["Sat"], antivoidcolor["Value"])
+		antivoidp.Position = Vector3.new(0, 23.5, 0)
+		antivoidp.Touched:connect(function(touchedvoid)
+			if touchedvoid.Parent:FindFirstChild("Humanoid") and touchedvoid.Parent.Name == lplr.Name then
+				lplr.Character.Humanoid.Jump = true
+				lplr.Character.Humanoid:ChangeState("Jumping")
+				wait(0.2)
+				lplr.Character.Humanoid:ChangeState("Jumping")
+				wait(0.2)
+				lplr.Character.Humanoid:ChangeState("Jumping")
+			end
+		end)
+	else
+		if antivoidp then
+			antivoidp:Remove()
+		end
+	end
+end)
+
+AntiVoid:NewColorPicker("Color", "Adjust antivoid color", Color3.fromHSV(antivoidcolor["Hue"], antivoidcolor["Sat"], antivoidcolor["Value"]), function(val)
+	if antivoidp then
+		antivoidp.Color = (val)
+	end
+end)
+
+AntiVoid:NewSlider("Invisible 1-100", "Adjust antivoid transparency", 100, 0, function(val)
+	if antivoidp then
+		antivoidp.Transparency = 1 - (val / 100)
+	end
+end)
+
+local UI = UI:NewSection("Colors")
+for theme, color in pairs(colors) do
+	UI:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+		kavoUi:ChangeColor(theme, color3)
+	end)
+end
